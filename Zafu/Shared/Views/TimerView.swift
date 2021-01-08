a4204462cea5c21f90c5509126998458029b11a7
Jeremie <jeremieberduck@icloud.com>
2021-01-08 10:12:29 +0100

    Copy

--- Zafu/Shared/Views/TimerView.swift
+++ Zafu/Shared/Views/TimerView.swift
@@ -44,7 +44,7 @@ struct TimerView: View {
                         }
                     }
             }
-        } .navigationBarTitle(Text("Zazen"))
+        }.navigationBarTitle(Text("Zafu"))
     }
 }
 
