3d4cde76f60822c5fffb2a93cbe15343ba273a57
Jeremie <jeremieberduck@icloud.com>
2021-01-08 09:36:47 +0100

    Initial XCode project

---
+++ Zafu/Shared/ZafuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZafuApp.swift
+//  Shared
+//
+//  Created by Jeremie Berduck on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ZafuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
