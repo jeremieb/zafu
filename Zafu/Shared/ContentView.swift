995828f2aa0caa6618341d27000749dbb0350cc0
Jeremie <jeremieberduck@icloud.com>
2021-02-02 15:31:48 +0100

    Background shape back

--- Zafu/Shared/ContentView.swift
+++ Zafu/Shared/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TimerView()
+        ZStack {
+            BackgroundView()
+            TimerView()
+        }
     }
 }
 
--- Zafu/Shared/Views/BackgroundView.swift
+++ Zafu/Shared/Views/BackgroundView.swift
@@ -13,7 +13,27 @@ struct BackgroundView: View {
 
             LinearGradient(gradient: gradientBackground, startPoint: .bottomTrailing, endPoint: .topLeading).ignoresSafeArea()
             
+            BottomShapeBack()
         }
+    }
+}
+
+struct BottomShapeBack: View {
+    var body: some View {
+        
+        GeometryReader { geometry in
+            
+            Path { path in
+                path.move(to: CGPoint(x: 0, y: geometry.size.height / 1.6 ))
+                path.addCurve(to: CGPoint(x: geometry.size.width, y: geometry.size.height / 2.6),
+                              control1: CGPoint(x: geometry.size.width / 3, y: geometry.size.height / 3.4),
+                              control2: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 1.6))
+                path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height / 8))
+                path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height))
+                path.addLine(to: CGPoint(x: 0, y: geometry.size.height))
+            }.fill(Color("shapes"))
+            
+        }.ignoresSafeArea()
     }
 }
 
