6eec81494dd0857d0d39175635ebcc988fda5582
Jeremie <jeremieberduck@icloud.com>
2021-02-05 14:22:42 +0100

    Adding quote images

---
+++ Zafu/Shared/Views/QuotesView.swift
@@ -0,0 +1,26 @@
+//
+//  QuotesView.swift
+//  Zafu (iOS)
+//
+//  Created by Jeremie Berduck on 05/02/2021.
+//
+
+import SwiftUI
+
+struct QuotesView: View {
+    var body: some View {
+        ZStack {
+            Image(systemName: "")
+            VStack {
+                
+                
+            }
+        }
+    }
+}
+
+struct QuotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuotesView()
+    }
+}
