d8ee80243cbe6ef23f68e5a76f7358ebdfecb0e5
Jeremie <jeremieberduck@icloud.com>
2021-02-05 15:55:44 +0100

    Adding quotes

--- Zafu/Shared/Utilities/Quotes.swift
+++ Zafu/Shared/Utilities/Quotes.swift
@@ -11,6 +11,12 @@ class MyQuotes {
     let quotes = [
         "Feelings come and go like clouds in a windy sky. Conscious breathing is my anchor." : "Thich Nhat Hahn",
         "To understand the immeasurable, the mind must be extraordinarily quiet, still." : "Jiddu Krishnamurti",
-        "Your goal is not to battle with the mind, but to witness the mind." : "Swami Muktananda"
+        "Your goal is not to battle with the mind, but to witness the mind." : "Swami Muktananda",
+        "Give the child a taste of meditation by creating a climate and atmosphere of love, acceptance and silence.":"Swami Dhyan Giten",
+        "Meditation is like a gym in which you develop the powerful mental muscles of calm and insight.":"Ajahn Brahm",
+        "The act of meditation is being spacious.":"Sogyal Rinpoche",
+        "We must experience the Truth in a direct, practical and real way. This is only possible in the stillness and silence of the mind; and this is achieved by means of meditation.":"Samael Aun Weor",
+        "Meditation and concentration are the way to a life of serenity.":"Baba Ram Dass",
+        "The more regularly and the more deeply you meditate, the sooner you will find yourself acting always from a center of peace.":"J. Donald Walters"
     ]
 }
