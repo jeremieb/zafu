14bd57576f3b6d5bf6efc179140667ec1bec7d50
Jeremie <jeremieberduck@icloud.com>
2021-02-18 15:58:19 +0100

    Adding basic settings view structure

--- Zafu/Shared/Views/SettingsView.swift
+++ Zafu/Shared/Views/SettingsView.swift
@@ -9,12 +9,86 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            /// Background color
+            Color.modalBackground.ignoresSafeArea()
+            
+            VStack {
+                /// Logo
+                VStack {
+                    Image("logo")
+                }.padding(.vertical)
+                
+                ScrollView(.vertical, showsIndicators: false){
+                    VStack(spacing: 10){
+                        
+                        // MARK: - ACHIEVEMENT SECTION
+                        HeaderSection(title: "Achievements")
+                        
+                        // MARK: - TIMER SETTINGS SECTION
+                        HeaderSection(title: "Timer")
+                        
+                        // MARK: - SYSTEM SECTION
+                        HeaderSection(title: "System")
+                        
+                        // MARK: - OTHERS SECTION
+                        HeaderSection(title: "Others")
+                        
+                        // MARK: - FOOTER
+                        FooterView()
+                    }
+                }
+                
+            } // end main VStack
+        } /// end main ZStack
+    }
+}
+
+struct HeaderSection: View {
+    var title: String
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                Color.mainColor.opacity(0.3)
+                HStack {
+                    Text(title)
+                        .font(.subheadline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.headersColor)
+                        .multilineTextAlignment(.leading)
+                        .textCase(.uppercase)
+                    Spacer()
+                }.padding(.horizontal)
+                
+            }.frame(minWidth: geo.size.width, minHeight: 20)
+        }
+    }
+}
+
+struct FooterView: View {
+    
+    private var heart = Image(systemName: "heart.fill")
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("Made with \(heart) in Amsterdam")
+                .font(.system(size: 12, weight: .semibold, design: .serif))
+                .foregroundColor(.mainColor)
+            Image("amsterdam")
+        }.padding(.vertical)
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        Group {
+            SettingsView()
+            SettingsView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
+
+
