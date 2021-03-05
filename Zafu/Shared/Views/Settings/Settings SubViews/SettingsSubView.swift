8522e0dc92fa699eba57d23cc26591664fb7c624
Jeremie <jeremieberduck@icloud.com>
2021-03-05 10:39:09 +0100

    cleaning

--- Zafu/Shared/Views/Settings/Settings SubViews/SettingsSubView.swift
+++ Zafu/Shared/Views/Settings/Settings SubViews/SettingsSubView.swift
@@ -11,7 +11,7 @@ struct SettingsSubView: View {
 
     var title: String = "Title placeholder"
     var icon: String = "seal.fill"
-    var content: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\nUt enim ad minim veniam, quis nostrud exercitation *ullamco* laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. **Excepteur sint** occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    var content: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         
     var body: some View {
         
