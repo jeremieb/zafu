f88479c64cf04394c33a4e7629e14a93512ca013
Jeremie <jeremieberduck@icloud.com>
2021-03-15 21:41:42 +0100

    Fixing timer selection… 😓

--- Zafu/Shared/Views/TimerView.swift
+++ Zafu/Shared/Views/TimerView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct TimerView: View {
-
+    
     @State var selectedButton = 0
     @State var timeRemaining = 0
     
@@ -21,111 +21,110 @@ struct TimerView: View {
     @AppStorage("secondTimer") var secondTimer: Int = 1200
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-
+    
     var body: some View {
         
-        return GeometryReader{ geometry in
+        // MARK: - TIMER IS NOT RUNNING
+        
+        if !myTimer.isStarted { /// Timer is not running
             
-            // MARK: - TIMER IS NOT RUNNING
-            
-            if !myTimer.isStarted { /// Timer is not running
+            ZStack {
                 
-                ZStack {
-
-                    VStack(alignment: .center) {
+                VStack(alignment: .center) {
+                    
+                    /// Define a spacer with the height of half of the screen
+                    Spacer().frame(height: UIScreen.main.bounds.height / 6)
+                    
+                    HStack {
                         
-                        /// Define a spacer with the height of half of the screen
-                        Spacer().frame(height: geometry.size.height / 6)
-                        
-                        HStack {
-                            
-                            /// 15 minutes
-                            VStack {
-                                Button(action: {
-                                    firstTimerSelected.toggle()
-                                    secondTimerSelected.toggle()
-                                }) {
-                                    TimerButtonView(time: firstTimer, isSelected: firstTimerSelected)
-                                }
-                                /// Selection Indicator
-                                Circle()
-                                    .frame(width: 12, height: 12, alignment: .center)
-                                    .opacity(firstTimerSelected == true ? 1 : 0 )
-                                    .foregroundColor(.mainColor)
+                        /// 15 minutes
+                        VStack {
+                            Button(action: {
+                                firstTimerSelected = true
+                                secondTimerSelected = false
+                                selectedButton = 0
+                            }) {
+                                TimerButtonView(time: firstTimer, isSelected: firstTimerSelected)
                             }
-                            
-                            Spacer()
-                            
-                            /// 20 minutes
-                            VStack {
-                                Button(action: {
-                                    secondTimerSelected.toggle()
-                                    firstTimerSelected.toggle()
-                                }) {
-                                    TimerButtonView(time: secondTimer, isSelected: secondTimerSelected)
-                                }
-                                /// Selection Indicator
-                                Circle()
-                                    .frame(width: 12, height: 12, alignment: .center)
-                                    .opacity(secondTimerSelected == true ? 1 : 0 )
-                                    .foregroundColor(.mainColor)
-                            }
+                            /// Selection Indicator
+                            Circle()
+                                .frame(width: 12, height: 12, alignment: .center)
+                                .opacity(firstTimerSelected ? 1 : 0 )
+                                .foregroundColor(.mainColor)
                         }
                         
-                    }.padding(.horizontal, 28).frame(width: geometry.size.width)
+                        Spacer()
+                        
+                        /// 20 minutes
+                        VStack {
+                            Button(action: {
+                                secondTimerSelected = true
+                                firstTimerSelected = false
+                                selectedButton = 1
+                            }) {
+                                TimerButtonView(time: secondTimer, isSelected: secondTimerSelected)
+                            }
+                            /// Selection Indicator
+                            Circle()
+                                .frame(width: 12, height: 12, alignment: .center)
+                                .opacity(secondTimerSelected ? 1 : 0 )
+                                .foregroundColor(.mainColor)
+                        }
+                    }
+                    
+                }.padding(.horizontal, 28).frame(width: UIScreen.main.bounds.width)
+                
+                VStack {
+                    Spacer()
+                    Button(action: {
+                        withAnimation(){
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                                startTimer(timer: selectedButton == 0 ? firstTimer : secondTimer)
+                                myTimer.isStarted = true
+                            }
+                            playSound(sound: "bell", type: "wav")
+                        }
+                    }) {
+                        Text("Start").font(.system(size: 22, weight: .heavy, design: .serif)).foregroundColor(.mainColor).textCase(.uppercase)
+                    }
+                    Spacer().frame(height: 30)
+                }
+            } // end ZStack
+            
+            // MARK: - TIMER IS RUNNING
+        } else {
+            
+            ZStack {
+                
+                VStack(alignment: .center) {
+                    
+                    SkyView()
+                    
+                    /// Running timer
+                    TimerLabelView(time: "\(timeString(time: TimeInterval(timeRemaining)))")
+                        .onReceive(timer) { _ in
+                            if timeRemaining > 0 {
+                                timeRemaining -= 1
+                            } else if timeRemaining == 0 {
+                                myTimer.isStarted = false
+                                playSound(sound: "bell", type: "wav")
+                            }
+                        }
                     
                     VStack {
                         Spacer()
                         Button(action: {
                             withAnimation(){
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                                    startTimer(timer: selectedButton == 0 ? firstTimer : secondTimer)
-                                    myTimer.isStarted = true
-                                }
-                                playSound(sound: "bell", type: "wav")
+                                myTimer.isStarted = false
+                                timeRemaining = 0
                             }
                         }) {
-                            Text("Start").font(.system(size: 22, weight: .heavy, design: .serif)).foregroundColor(.mainColor).textCase(.uppercase)
+                            Text("Stop").font(.system(size: 22, weight: .heavy, design: .serif)).foregroundColor(.mainColor).textCase(.uppercase)
                         }
                         Spacer().frame(height: 30)
                     }
-                } // end ZStack
-                
-                // MARK: - TIMER IS RUNNING
-            } else {
-                
-                ZStack {
-                    
-                    VStack(alignment: .center) {
-
-                        SkyView()
-                        
-                        /// Running timer
-                        TimerLabelView(time: "\(timeString(time: TimeInterval(timeRemaining)))")
-                            .onReceive(timer) { _ in
-                                if timeRemaining > 0 {
-                                    timeRemaining -= 1
-                                } else if timeRemaining == 0 {
-                                    myTimer.isStarted = false
-                                    playSound(sound: "bell", type: "wav")
-                                }
-                            }
-                        
-                        VStack {
-                            Spacer()
-                            Button(action: {
-                                withAnimation(){
-                                    myTimer.isStarted = false
-                                    timeRemaining = 0
-                                }
-                            }) {
-                                Text("Stop").font(.system(size: 22, weight: .heavy, design: .serif)).foregroundColor(.mainColor).textCase(.uppercase)
-                            }
-                            Spacer().frame(height: 30)
-                        }
-                    }
-                    .frame(width: geometry.size.width, alignment: .center)
                 }
+                .frame(width: UIScreen.main.bounds.width, alignment: .center)
             }
         }
     }
