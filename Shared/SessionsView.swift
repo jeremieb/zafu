7dae5c8333e5318a2c5916895c473873bc4ad9e2
Jeremie <jeremieberduck@icloud.com>
2021-05-29 16:30:09 +0200

    Fake cells

--- Shared/SessionsView.swift
+++ Shared/SessionsView.swift
@@ -29,17 +29,63 @@ struct SessionsView: View {
 
 struct CellText: View {
     
-    var colors = [Color(UIColor.systemIndigo), Color(UIColor.systemPink), Color(UIColor.systemGreen)]
+    @Environment(\.colorScheme) var colorScheme
+    
+    var colors = [Color.mainOrange, Color.mainSky, Color.mainDarkPink, Color.mainBlue, Color.mainPink]
+    var duration = [5,10,25,30,45]
+    var title = "Title"
+    var description = [
+        "Cras justo odio, dapibus ac facilisis in, egestas eget quam.",
+        "Curabitur blandit tempus porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Morbi leo risus, porta ac consectetur ac, vestibulum at eros.",
+        "Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
+    ]
+    
+    var colorComputed: Color {
+        return colors.randomElement() ?? Color.mainOrange
+    }
+    
+    var durationComputed: Int {
+        return duration.randomElement() ?? 5
+    }
+    
+    var descriptionComputed: String {
+        return description.randomElement() ?? ""
+    }
     
     var body: some View {
-        VStack(alignment: .leading){
-            Text("Test")
-            Text("Curabitur blandit tempus porttitor.")
+        /// Content
+        HStack {
+            VStack(alignment: .leading){
+                Text(String(durationComputed) + " min")
+                    .font(.caption)
+                    .foregroundColor(colorComputed)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 3)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(colorComputed, lineWidth: 1)
+                    )
+                    .lineLimit(1)
+                Spacer()
+                Text(title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(colorComputed)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                Text(descriptionComputed)
+                    .font(.footnote)
+                    .foregroundColor(colorComputed)
+                    .lineLimit(2)
+            }
+            .padding()
+            Spacer()
         }
-        .frame(width: UIScreen.main.bounds.size.width - 64)
-        .padding()
-        .background(colors.randomElement()?.opacity(0.1))
-        .cornerRadius(20)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(colorComputed).opacity(colorScheme == .dark ? 0.1 : 0.8))
+        .frame(width: UIScreen.main.bounds.size.width - 32, height: 150)
     }
 }
 
